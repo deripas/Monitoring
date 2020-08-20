3281db005b328963204c999ca1eaf22668aa9d2c
Anton Deripaska <a.deripaska@semrush.com>
2020-08-20 23:31:32 +0300

    add empty web server

---
+++ Server/Api/ApiController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Api
+{
+    [ApiController]
+    [Route("/api")]
+    [Produces("application/json")]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("echo")]
+        public List<string> echo()
+        {
+            return new List<string>(){"1", "2"};
+        }
+    }
+}
