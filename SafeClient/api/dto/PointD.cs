9d6dfb2bd82d57a8dc43a9714511012fafb71ad7
Anton Deripaska <a.deripaska@semrush.com>
2020-08-22 23:48:38 +0300

    add new

---
+++ SafeClient/api/dto/PointD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace api.dto
+{
+    public class PointD
+    {
+        public double time { get; set; }
+
+        public double value { get; set; }
+    }
+}
