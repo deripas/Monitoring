6673e2ade03b0416aa445fa37a0e8a2495574ca2
Anton Deripaska <a.deripaska@semrush.com>
2020-08-30 20:40:06 +0300

    add camera service

---
+++ Server/service/CameraService.cs
@@ -0,0 +1,31 @@
+﻿using SafeServer.dto;
+using SafeServer.service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.service
+{
+    public class CameraService
+    {
+        public Camera this[int id] => map[id];
+        private Dictionary<int, Camera> map;
+
+        public CameraService()
+        {
+            map = new Dictionary<int, Camera>();
+        }
+
+        public void Init()
+        {
+            using var db = new DatabaseService();
+            map = db.Camera.ToDictionary(c => c.Id);
+        }
+
+        internal void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
